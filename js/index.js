e7bea5d0cce5411127d718f7d2efff1890e4ebd4
孙双全 <sunshuangquan@qutoutiao.net>
2019-05-06 12:02:53 +0800

    feat: 相对路径错误

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-load("common/header.html","#headerTop");
-load("common/footer.html","#footer")
+load("./html/common/header.html","#headerTop");
+load("./html/common/footer.html","#footer")
 
 $('.ck-slide').ckSlide({
 	autoPlay: true
